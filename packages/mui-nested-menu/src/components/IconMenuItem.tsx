f1d07312084706fd0fdb8590b65a84da29607add
Anne Lin <100283618+anne-lin-magna@users.noreply.github.com>
2026-01-14 12:10:59 +0200

    SxProps fix

--- packages/mui-nested-menu/src/components/IconMenuItem.tsx
+++ packages/mui-nested-menu/src/components/IconMenuItem.tsx
@@ -1,8 +1,8 @@
 import MenuItem, { MenuItemProps } from '@mui/material/MenuItem';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import { SxProps } from '@mui/system';
 import Box from '@mui/system/Box';
-import { SxProps } from '@mui/system/styleFunctionSx/styleFunctionSx';
 import React, { forwardRef, RefObject } from 'react';
 
 const StyledMenuItem = styled(MenuItem)({
--- packages/mui-nested-menu/src/definitions.ts
+++ packages/mui-nested-menu/src/definitions.ts
@@ -1,4 +1,4 @@
-import { SxProps } from '@mui/system/styleFunctionSx/styleFunctionSx';
+import { SxProps } from '@mui/system';
 
 export interface MenuItemData {
     uid?: string;
